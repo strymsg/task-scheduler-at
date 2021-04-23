5cd52431b129f7e3e3af0cdb4e55c12afc03ec2f
Rodrigo Garcia <Rodrigo.Garcia@jala-foundation.org>
2021-04-23 10:57:48 -0400

    Added test cases and basic failure

--- tests/tasks/test_apitask.py
+++ tests/tasks/test_apitask.py
@@ -23,6 +23,22 @@ def config_api_post():
     return config
 
 @pytest.fixture
+def config_api_patch():
+    config = ConfigApiRequestTask(
+        'https://httpbin.org/patch',
+        http_method='patch',
+        headers={ 'Accept': 'application/json', 'Content-Type': 'application/json'},
+        body={ 'key': 'value' })
+    return config
+
+@pytest.fixture
+def config_api_delete():
+    config = ConfigApiRequestTask(
+        'https://httpbin.org/delete',
+        http_method='delete')
+    return config
+
+@pytest.fixture
 def api_task(config_api_get):
     apitask = ApiRequestTask(0, config=config_api_get)
     return apitask
@@ -32,7 +48,6 @@ def test_apitask(api_task):
 
 def test_get_request1(api_task):
     resp = api_task.execute()
-    #print(resp, type(resp))
     assert resp['url'] == 'https://api.github.com/'
     expected_keys = ['json', 'text','status_code','headers','url']
     for expected_key in expected_keys:
@@ -43,3 +58,30 @@ def test_post_request(config_api_post):
     resp = api_task.execute()
     # print(resp)
     assert resp['status_code'] == 200
+
+def test_patch_request(config_api_patch):
+    api_task = ApiRequestTask(0, config=config_api_patch)
+    resp = api_task.execute()
+    assert resp['status_code'] == 200
+
+def test_delete_request(config_api_delete):
+    api_task = ApiRequestTask(0, config=config_api_delete)
+    resp = api_task.execute()
+    assert resp['status_code'] == 200
+
+def test_get_notfound():
+    config = ConfigApiRequestTask('https://api.github.com/inex', http_method='get')
+    api_task = ApiRequestTask(0, config=config)
+    resp = api_task.execute()
+    assert resp['status_code'] == 404
+
+def test_request_fail1():
+    config = ConfigApiRequestTask('https://api.nutexis.cualquiercosa', http_method='get')
+    api_task = ApiRequestTask(0, config=config)
+    try:
+        resp = api_task.execute()
+        assert resp is None
+    except ConnectionError as conn_err:
+        assert True
+    except Exception as err:
+        assert type(err) != "<class 'requests.exceptions.ConnectionError'>"
