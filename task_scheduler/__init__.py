9165dd1423ac20e83da0d9e3396c2cf4b7d9680a
Rodrigo Garcia <Rodrigo.Garcia@jala-foundation.org>
2021-04-29 14:54:09 -0400

    Adding basic uitls and init with flask

---
+++ task_scheduler/__init__.py
@@ -0,0 +1,30 @@
+"""
+Task Manager and Scheduler
+"""
+
+__version__ = "0.0.1"
+import os
+import logging
+from flask import Flask
+
+from task_scheduler.configs.config import Configuration
+from task_scheduler.utils.logger import CustomLogger
+
+def config_logger(config):
+      logger = CustomLogger(__name__, config)
+      return logger
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    print("task scheduler...")
+    config_obj = Configuration()
+
+    logger = config_logger(config_obj.configuration)
+    logger.info("logging info test")
+    logger.error("logging Error test")
+    logger.debug("logging debug test")
+    app.config.from_mapping(
+        SECRET_KEY=config_obj.get_config_var('secret_key'),
+    )
+
+    return app
