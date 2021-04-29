9684571a134ba61759829b2f4157f85d1017f06f
Rodrigo Garcia <Rodrigo.Garcia@jala-foundation.org>
2021-04-29 11:34:37 -0400

    Adding sample config files

---
+++ task_scheduler/configs/config.py
@@ -0,0 +1,32 @@
+# there should be a configuration class that can return data about:
+#- the db port of my db for my application
+#- the ip/hostname of my db db for my application
+#- the log level for my custom logger (this is for the Customlogger to consume)
+import os
+import json
+from json import JSONDecodeError
+
+class Configuration:
+    '''Loads the configuration json file (depending on FLASK_ENV variable)
+     and returns a dict. If loading fails prints error and returns enmpty dict
+    '''
+    def __init__(self):
+        self.configuration = {}
+        filename = ''
+        if os.environ.get('FLASK_ENV') == 'development':
+            filename = 'task_scheduler/configs/config.dev.json'
+        else:
+            filename = 'task_scheduler/configs/config.json'
+
+        with open("task_scheduler/configs/config.dev.json") as jsonfile:
+            try:
+                d = json.loads(jsonfile.read())
+                self.configuration = d
+            except JSONDecodeError as err:
+                print(f"Error decoding json {jsonfile.name}")
+                print(err)
+            return self.configuration
+
+    def get_config_var(self, var_name, default_value=None):
+        return self.configuration.get(var_name, default_value)
+
